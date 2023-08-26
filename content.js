d02b89c0eef06798b644b33c5fbfee9c12f4e6f1
Agastya-07 <sharmaagastya0709@gmail.com>
2023-08-26 23:34:19 +0530

    new updates

--- content.js
+++ content.js
@@ -2,9 +2,16 @@ const canvas = document.getElementById("drawingCanvas");
 const context = canvas.getContext("2d");
 const clearbutton = document.getElementById("clearbutton");
 const parent  = document.querySelector("#colorpicker")
-
+const moon = parent.querySelector("#moon")
+const sun = parent.querySelector("#sun")
 const red = parent.querySelector("#red")
 const green =  parent.querySelector("#green")
+const blue =  parent.querySelector("#blue")
+const yellow =  parent.querySelector("#yellow")
+const pink =  parent.querySelector("#pink")
+const credit = document.querySelector(".credit")
+
+
 
 // Resize the canvas to match the viewport size
 canvas.width = window.innerWidth;
@@ -32,7 +39,14 @@ canvas.addEventListener("mouseup", () => {
 });
 
 function clearcanvas(){
+  if (ismoon){
     context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "black";
+    context.fillRect(0,0,canvas.width,canvas.height)
+  }
+  else{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 clearbutton.addEventListener("click", clearcanvas);
@@ -51,6 +65,15 @@ function getMousePos(canvas, evt) {
 red.addEventListener("click", () => {
     context.strokeStyle = "red";
 })
+blue.addEventListener("click", () => {
+    context.strokeStyle = "blue";
+})
+pink.addEventListener("click", () => {
+    context.strokeStyle = "pink";
+})
+yellow.addEventListener("click", () => {
+    context.strokeStyle = "yellow";
+})
 
 green.addEventListener("click", () => {
     context.strokeStyle = "green";
@@ -58,3 +81,30 @@ green.addEventListener("click", () => {
 black.addEventListener("click", () => {
     context.strokeStyle = "black";
 })
+let ismoon = false;
+let issun = false;
+moon.addEventListener("click", () => {
+  ismoon = true;
+  issun = false;
+  document.body.style.backgroundColor = "black";
+  context.strokeStyle = "white";
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+})
+
+sun.addEventListener("click",()=>{
+  issun = true;
+  ismoon = false;
+  document.body.style.backgroundColor = "white";
+  context.strokeStyle = "black";
+  clearbutton.style.color = "black";
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+})
+
+if(ismoon){
+  credit.style.color="yellow"
+}
+else{
+  credit.style.color="black"
+}
